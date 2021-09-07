6bf89d9f060f3b83a555f38ca53b90fbc1a4381f
SIN88 <KG19882020@GMAIL.COM>
2021-09-06 20:08:45 -0700

    spaced out links in the footer/all links work

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import './Footer.css'
 const Footer = ({footing,light}) => {
-    return       <footer className="shadow-lg  p-2 mb-2 bg rounded">
+    return       <footer className="shadow-sm  p-1 mb-1 bg rounded">
     <h1>{footing || 'PORTFOLIO'}</h1>
         <a href="https://www.linkedin.com/in/kevin-giron-869598202/ ">Linkedin</a>
         <a href="mailto:kg19882020@gmail.com">Email Me</a>
