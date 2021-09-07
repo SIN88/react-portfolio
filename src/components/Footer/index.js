87e5495497539b83bc139c2f23d2bd293fdebb2b
SIN88 <KG19882020@GMAIL.COM>
2021-09-06 19:48:10 -0700

    quick save

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import './Footer.css'
 const Footer = ({footing,light}) => {
     return       <footer className="shadow-lg  p-2 mb-2 bg rounded">
     <h1>{footing || 'PORTFOLIO'}</h1>
-    <a href="https://www.linkedin.com/in/kevin-giron-869598202/ ">Linkedin</a>
+        <a href="https://www.linkedin.com/in/kevin-giron-869598202/ ">Linkedin</a>
         <a href="mailto:kg19882020@gmail.com">Email Me</a>
         <a href="https://github.com/SIN88">Github</a>
   </footer>
