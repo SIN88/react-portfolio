a973e083b419c05857e6aa25bd927b81ca06d910
SIN88 <KG19882020@GMAIL.COM>
2021-09-09 04:41:14 -0700

    added a profile img

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,7 @@ import './About.css'
 const About  =() => {
     return <>
     <h1>About Me</h1>
+    <img src="/images/profilepic.jpg" alt=""/>
     <p>
      Greetings!!! My name is Kevin. I am a web developer in Huntington Harbour, California.
      Looking implement and grow my newly aquired skills.
