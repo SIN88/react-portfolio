4945f5b68818c2d11340416c7caf9d0575d417bc
SIN88 <KG19882020@GMAIL.COM>
2021-09-09 03:56:34 -0700

    renamed the navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,25 +10,23 @@ const MyNav  =() => {
     return <>
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">🤖</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+      <Nav.Link href="#About">About</Nav.Link>
+      <Nav.Link href="#Contact">Contact</Nav.Link>
+      <Nav.Link href="#Skills">Skills</Nav.Link>
+      <NavDropdown title="Projects" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#Project1">Project1</NavDropdown.Item>
+        <NavDropdown.Item href="#Project2">Project2</NavDropdown.Item>
+        <NavDropdown.Item href="#WorkingApps">Working App</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
+      
     </Nav>
   </Navbar.Collapse>
   </Container>
