7e0c95e61b0b882648c1dadfc4084fd0613ecbb0
SIN88 <KG19882020@GMAIL.COM>
2021-09-09 06:25:53 -0700

    added skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,9 +1,27 @@
 const Skills=()=>{
     return  <>
 <h1 id ="Skills">Skills</h1>
-<p>
- these are the skills i got 
-</p>
+<h2>Front-end Proficiencies</h2>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>responsive design</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+
+          <h2>Back-end Proficiencies</h2>
+        
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL, Sequelize</li>
+          <li>MongoDB, Mongoose</li>
+          <li>REST</li>
+          <li>GraphQL</li>
+
+
+
 </>
 }
 
