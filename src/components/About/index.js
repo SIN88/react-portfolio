755bd0b83179914fabebf98f521aae87ef44e6c7
SIN88 <KG19882020@GMAIL.COM>
2021-09-10 20:52:40 -0700

    profile pic is not working

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,18 @@
-import './About.css'
+import './About.css';
+import profilePic from'./src/Images/profilePic.jpg';
+
+//import imageName from '../../assets/images/imageName.jpg';
+   
+// inside return 
+<img src={profilePic} alt=""/>
 
 const About  =() => {
     return <>
     <h1 id="About">About Me</h1>
-    <img src="/images/profilepic.jpg" alt=""/>
+    {/*<img src="/images/profilepic.jpg" alt=""/>*/}
+    {/*<img src={"profilepic.jpg"} alt=""/>*/}
+    
+    
     <p>
      Greetings!!! My name is Kevin. I am a web developer in Huntington Harbour, California.
      Looking implement and grow my newly aquired skills.
