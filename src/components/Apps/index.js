faebece57cbe4df282e5f036c0746308dda54301
SIN88 <KG19882020@GMAIL.COM>
2021-09-12 15:02:07 -0700

    added last apps

--- src/components/Apps/index.js
+++ src/components/Apps/index.js
@@ -1,7 +1,10 @@
 import './Apps.css'
 import fitnessTracker from"../../Images/fitnessTracker.jpg";
-import fitnessTracker2 from"../../Images/fitnessTracker2.jpg";
-import { Image } from 'react-bootstrap';
+ import fitnessTracker2 from"../../Images/fitnessTracker2.jpg";
+ import BudgetTracker from"../../Images/BudgetTracker.gif";
+ import Generator from"../../Images/Generator.gif";
+ import Last from"../../Images/Last.jpg";
+ import { Image } from 'react-bootstrap';
 
 
 const Apps = () => {
@@ -15,25 +18,47 @@ const Apps = () => {
 
     <div className="card">
   <Image src={fitnessTracker } className="card-img-top" alt="..."/>
-  <Image src={fitnessTracker2} className="card-img-top" alt="..."/>
+   <Image src={fitnessTracker2} className="card-img-top" alt="..."/> 
 
   <div className="card-body">
     <p className="card-text"><a href="https://frozen-ravine-00061.herokuapp.com/?id=613da5fbf20ada00161e22fc"> Fitness Tracker </a>
-    This app tracks your fitness workouts on and offline with a service worker, also it is deployed using heroku .</p></div>
+    This app tracks your fitness workouts on and offline with a service worker, also it is deployed using heroku .<a href="https://github.com/SIN88/Fitness-Tracker18.git">GitHub</a></p></div>
 </div>
 
 
     
-    {/* <div className="card"> */}
+     <div className="card"> 
   
-  {/* <Image src={game} fluid className="card-img-top" alt ="..." /> */}
-  {/* <div className="card-body"> */}
-    {/* <p className="card-text"><a href="https://project2nrk.herokuapp.com/">NRK</a> This application is a game built with Phaser. */}
-                         {/* It has alternating platforms with different levels of difficulty, a login page, and a high score keeper .</p> */}
+  <Image src={BudgetTracker} fluid className="card-img-top" alt ="..." /> 
+   <div className="card-body"> 
+     <p className="card-text"><a href="https://offline-budget-kg.herokuapp.com/">Progressive Budget Tracker</a> This is a budget tracker app that works online 
+    and offline deployed to heroku .<a href="https://github.com/SIN88/budget-offline.git">Github</a></p> 
 
  </div>
 
-    
+     <div className="card"> 
+  
+   <Image src={Generator} fluid className="card-img-top" alt ="..." /> 
+   <div className="card-body"> 
+     <p className="card-text"><a href=" https://github.com/SIN88/09-homework.git/">Read Me Generator</a> A read me Generator .</p> 
+
+                        
+                             <div className="card"> 
+  
+   <Image src={Last} fluid className="card-img-top" alt ="..." />
+   <div className="card-body"> 
+     <p className="card-text"><a href="https://github.com/SIN88/week12latehomework.git">Employee Management System</a> This application is made to add,delete and manage employee roles .</p> 
+
+
+   
+                        
+
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
        
     
   
