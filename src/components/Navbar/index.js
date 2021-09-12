6f6a165976a807f3c3c4af8294e0dae34782cde7
SIN88 <KG19882020@GMAIL.COM>
2021-09-12 16:52:48 -0700

    fixed the nav bar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,15 +10,15 @@ const MyNav  =() => {
     return <>
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="/Home">🤖</Navbar.Brand>
+  <Navbar.Brand href="#Home">🤖</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="/About">About</Nav.Link>
-      <Nav.Link href="/Contact">Contact</Nav.Link>
-      <Nav.Link href="/Skills">Skills</Nav.Link>
-      <Nav.Link href="/Projects">Projects</Nav.Link>
-      <Nav.Link href="/Apps">Apps</Nav.Link>
+      <Nav.Link href="#About">About</Nav.Link>
+      <Nav.Link href="#Contact">Contact</Nav.Link>
+      <Nav.Link href="#Skills">Skills</Nav.Link>
+      <Nav.Link href="#Projects">Projects</Nav.Link>
+      <Nav.Link href="#Apps">Apps</Nav.Link>
       
         {/* <NavDropdown.Item href="/Projects">Projects</NavDropdown.Item> */}
         {/* <NavDropdown.Item href="/Projects">Project2</NavDropdown.Item> */}
