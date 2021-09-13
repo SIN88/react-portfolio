2a2c69d96ba884d9b3a81a7ed34fc6d9b83f50c1
SIN88 <KG19882020@GMAIL.COM>
2021-09-12 17:32:52 -0700

    added seperate pgs opens blank

--- src/App.js
+++ src/App.js
@@ -6,66 +6,74 @@ import Wave from "./components/Wave";
 import Wave2 from "./components/Wave2";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Navbar from "./components/Navbar"
-import Apps from "./components/Apps"
+import Navbar from "./components/Navbar";
+import Apps from "./components/Apps";
+// import Home from "./components/Home"
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+ } from "react-router-dom";
 
-
-function App() {
+  function App() {
   return (
-    <>
+    <Router>
+      <div>
+        <Wave />
       <Header heading={"Kevin Giron"} />
-      
-      <main>
-        <Navbar/>
-        {/* <Section light title={'About Me'} />
-        <Section />
-        <Section light />
-        <Section /> */}
-        {/* <section className="dark">
-          <h1>Nice Curves!</h1>
-          <p>
-            A website is like a road. The more curves it has the more
-            interesting it is.
-          </p>
-        </section> */}
-        
 
-        <section className="light">
-         
-        <About/>
-          
-        </section>
-        
-        
-        <section className="dark">
-         
-        <Contact/>
-           <Wave/>
-         </section>
+        <Navbar>
+          <ul>
+            {/* <li>
+              <Link to="/Home">🤖</Link>
+            </li>
+           */}
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+            <li>
+            <Link to="/Skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="/Projects">About</Link>
+            </li>
+            <li>
+              <Link to="/Apps">Apps</Link>
+            </li>
+        </ul>
+        </Navbar>
 
-        <section className="light">
-          <Projects/>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        {/* <section className="light"> */}
+        <Route  exact   path="/About"  render={() => <About />}/>
+        {/* </section> */}
+        {/* <Route path="/about" ={About} /> */}
+          {/* <Route path="/About"> */}
+            {/* <About /> */}
+          {/* </Route> */}
+          {/* <Route path="/Contact"> */}
+          <Route  exact   path="/Contact"  render={() => <Contact />}/>
+            {/* <Contact /> */}
+          {/* </Route> */}
+          <Route  exact   path="/Skills"  render={() => <Skills />}/>
 
-         <Wave2/>
-        </section>
-        
-       
-        <section className="dark">
-         <Skills/>
-         
-          <Wave/>
-        </section>
+          {/* <Route path="/Skills"> */}
+            {/* <Home /> */}
+          {/* </Route> */}
+          <Route  exact   path="/Projects"  render={() => <Projects />}/>
+          <Route  exact   path="/Apps"  render={() => <Apps />}/>
 
-        <section className="light">
-          <Apps/>
-
-          <Wave2/>
-        </section>
-       
-      </main>
-      
-      <Footer footing={"Made with ☕"} />
-    </>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
